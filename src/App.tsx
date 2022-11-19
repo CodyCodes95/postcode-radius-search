0160f573ef83194edc632f13a80a7d8b1de6cd3c
Cody Partington <codythatsme@gmail.com>
2022-11-19 14:58:26 +1100

    basic UI set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import * as React from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 const App = () => {
 
+  const [location, setLocation] = React.useState("")
+  const [radius, setRadius] = React.useState("")
+
+  const handleSubmit = (e: React.MouseEvent) => {
+    
+  };
+
   return (
-    <div>
-     
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-slate-900 text-white">
+      <div className="w-[50%] flex flex-col justify-center items-center">
+        <div className='flex'>
+          <input type="text" value={location} placeholder="Enter a location" />
+          <input type="text" value={radius as string} placeholder="Enter a radius" />
+        </div>
+        <button onClick={handleSubmit}>Find Postcodes</button>
+      </div>
+      <footer className="w-full flex items-center justify-center sticky top-[100vh]">
+        <a className="p-2" href="">
+          Github
+        </a>
+        <a className="p-2" href="">
+          Twitter
+        </a>
+      </footer>
     </div>
-  )
+  );
 }
 
 export default App
